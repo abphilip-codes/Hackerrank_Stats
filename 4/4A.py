a81e0d93d65874ebc161be69e7750c27ecfd5806
Allen <allenalvin333@gmail.com>
2021-06-19 12:00:21 +0530

    programs

--- 4/4A.py
+++ 4/4A.py
@@ -5,7 +5,7 @@ x,y = list(map(float, input().split(" ")))
 odds = x/y
 answer = 0
 for z in range(3,7):
-    temp1 = factorial(6) / (factorial(z) * factorial(6-z))
-    temp2 = temp1*((odds/(1+odds)**z))*((1-(odds/(1+odds))**(6-z)))
+    temp1 = factorial(6)/(factorial(z)*factorial(6-z))
+    temp2 = temp1*((odds/(1+odds))**z)*((1-(odds/(1+odds)))**(6-z))
     answer = answer+temp2
 print(round(answer,3))
