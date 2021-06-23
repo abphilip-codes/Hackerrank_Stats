94612091b4ed1da1f41092232d73b8e68498554c
Allen <allenalvin333@gmail.com>
2021-06-23 05:42:17 +0530

    programs

--- 4/4D.py
+++ 4/4D.py
@@ -1,10 +1,9 @@
-def geo_dist(n, p):
-    return (1-p)**(n-1) * p
-
 x, y = map(int, input().split())
 p = x/y
-answer = 0
-for z in range(1,n+1):
-    answer = answer + temp
 n = int(input())
-print(round(sum([geo_dist(i, p) for i in range(1,n+1)]),3))
+answer = 0
+
+for z in range(1,n+1):
+    temp = (1-p)**(z-1) * p
+    answer = answer + temp
+print(round(answer,3))
