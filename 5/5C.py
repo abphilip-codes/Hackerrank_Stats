3d1c4c88fd0c1249a7a9296adb4277a5888ab3a1
Allen <allenalvin333@gmail.com>
2021-06-24 06:18:03 +0530

    programs

--- 5/5C.py
+++ 5/5C.py
@@ -1,8 +1,11 @@
 # https://www.hackerrank.com/challenges/s10-geometric-distribution-1/problem
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-x,y=map(int,input().split())
-z=int(input())
-p=x/y
-q=1-p
-print(round((q**(z-1))*p,3))
+from math import erf
+m, sd = list(map(int, input().split(" ")))
+q1 = float(input())
+q2a, q2b = list(map(float, input().split(" ")))
+p1 = 0.5 + 0.5 *erf((q1-m)/(sd*2**0.5))
+p2 = 0.5*erf((q2b-m)/(sd*2**0.5)) - 0.5*erf((q2a-m)/(sd*2**0.5))
+print(round(p1,3))
+print(round(p2,3))
