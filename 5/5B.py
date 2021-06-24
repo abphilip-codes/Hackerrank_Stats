4f8f86c144f2dfe7031b286b4728a988e16f4a1b
Allen <allenalvin333@gmail.com>
2021-06-24 06:07:36 +0530

    programs

--- 5/5B.py
+++ 5/5B.py
@@ -1,18 +1,8 @@
-# https://www.hackerrank.com/challenges/s10-binomial-distribution-2/problem
+# https://www.hackerrank.com/challenges/s10-poisson-distribution-2/problem
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-def factorial(num):
-    return 1 if num == 0 else num*factorial(num-1)
-answer1 = 0
-answer2 = 0
-
-x,y = list(map(int, input().split(" ")))
-p = x/100
-for z in range(3):
-    temp1 = (factorial(y) / (factorial(z) * factorial(y-z))) * p**z * (1-p)**(y-z)
-    answer1 = answer1 + temp1
-print(round(answer1,3))
-for z in range(2, y+1):
-    temp2 = (factorial(y) / (factorial(z) * factorial(y-z))) * p**z * (1-p)**(y-z)
-    answer2 = answer2 + temp2
-print(round(answer2,3))
+x,y = list(map(float, input().split(" ")))
+cost1 = 160 + 40*(x+x**2)
+cost2 = 128 + 40*(y+y**2)
+print(round(cost1, 3))
+print(round(cost2, 3))
