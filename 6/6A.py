2c28f7c86e457279cf24f5ef5c5808ceeecde2f8
Allen <allenalvin333@gmail.com>
2021-06-25 16:35:17 +0530

    programs

--- 6/6A.py
+++ 6/6A.py
@@ -4,17 +4,10 @@
 import math
 x = int(input())
 n = int(input())
-μ = int(input())
-σ = int(input())
+mu = int(input())
+si = int(input())
 
-mu = n*μ
-s = math.sqrt(n)*σ)
-
-mu_sum = n * mu 
-sigma_sum = math.sqrt(n) * sigma
-
-def cdf(x, mu, sigma):
-    Z = (x - mu)/sigma
-    return 0.5*(1 + math.erf(Z/(math.sqrt(2))))
-
-print(round(cdf(x, mu_sum, sigma_sum), 4))
+m = n*mu
+s = math.sqrt(n)*si
+Z = (x-m)/s
+print(round(0.5*(1+math.erf(Z/math.sqrt(2))),4))
